0472d56217ae842fdcec45477521e0a2e56d501d
Kevincorco1988 <kevincorco1988@gmail.com>
2022-01-17 11:17:48 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
 employee = {"name": "David", "argue": 10, "rejuvenate_determination": 15, "determination": 100}
-horrible_boss = {"name": "Chris", "argue": 12, "determination": 100}
-arguement_running = True
+horrible_boss = {"name": "Chris", "argue": 13, "determination": 100}
+meeting_running = True
 
-while arguement_running == True:
+while meeting_running == True:
+    """
+    Get employee action selection from the user
+    """
+    employee_won = False
+    horrible_boss_won = False
 
     print("Please select action")
     print("1) Argue")
     print("2) Rejuvenate Determination")
 
-    employee_action_selection = input()
+    employee_action_selection = input(" :\n")
 
 
     if employee_action_selection == "1":
+        """
+        Calculates the determination of the user and computer
+        to decide who wins based on the action chosen
+        """
         horrible_boss["determination"] = horrible_boss["determination"] - employee["argue"]
-        employee["determination"] = employee["determination"] - horrible_boss["argue"]
-        
+        if horrible_boss["determination"] <= 0:
+            employee_won = True
+
+        else:
+            employee["determination"] = employee["determination"] - horrible_boss["argue"]
+            if employee["determination"] <= 0:
+                horrible_boss_won = True
+            
         print(horrible_boss["determination"])
         print(employee["determination"])
 
@@ -24,5 +39,8 @@ while arguement_running == True:
     else: 
         print("Invalid Input")
 
-    if employee["determination"] <= 0:
-        arguement_running = False
+    if employee_won == True or horrible_boss_won == True:
+        """
+        Decides the outcome of the meeting
+        """
+        meeting_running = False
